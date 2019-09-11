2c945113bfc26cef0985fc1a733a85cea6f839a9
Henrique Sant'Anna <hsantanna@gmail.com>
2019-09-11 12:28:34 -0300

    olamundo de mapeamento urls / views

--- encurtador/views.py
+++ encurtador/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
 
 # Create your views here.
+# view baseada em função (FBV)
+def function_based_view(request, *args, **kwargs):
+    return HttpResponse("Olá mundo!")
+
+
+# view baseada em classe (CBV)
+class UrlCBView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("olá novamente!")
--- urlcurta/urls.py
+++ urlcurta/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from encurtador.views import function_based_view, UrlCBView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('olamundo/', function_based_view),
+    path('oladenovo/', UrlCBView.as_view()),
 ]
